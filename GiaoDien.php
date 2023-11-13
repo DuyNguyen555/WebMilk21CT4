a79a4b5d75ade608c4dcf21e0e222cf3f9e3ba7d
ThanhHo2205 <thanhcongho880@gmail.com>
2023-11-13 13:11:43 +0700

    First commit

---
+++ GiaoDien.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>SALE MILK</title>
+</head>
+<body>
+ <header>
+    <div class="logo">
+        <img src="images/logo1.jpg" alt="logo milk">
+    </div>
+    <div class="menuon">
+        <li><a href="">Trang chủ</a></li>
+        <li><a href="">Giới thiệu</a></li>
+        <li><a href="">Hãng</a></li>
+        <li><a href="">Khuyến mãi</a></li>
+        <li><a href="">Liên hệ</a></li>
+    </div>
+    <div class="option">
+        <li><input type ="text"><i class = "fas fa-search"></i></li>
+        <li><a class = "fas fa-user"></a></li>
+        <li><a class = "fas fa-shopping-cart"></a></li>
+    </div>
+ </header>   
+</body>
+</html>
