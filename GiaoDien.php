24c943b0c7081bd1724d1f9a12a986d491a4ab77
ThanhHo2205 <thanhcongho880@gmail.com>
2023-11-14 00:59:33 +0700

    footer

--- GiaoDien.php
+++ GiaoDien.php
@@ -48,20 +48,50 @@
     <div class="dot"></div>
 </div>
 </section>
-</body>
+<section class="app-container">
+    <p>Tải ứng dụng DTC MILK</p>
+    <div class="app-google">
+        <img src="images/dow.webp">
+    </div>
+</section>
+<div class="footer-top">
+    <li><a href="">Liên hệ</a></li>
+    <li><a href="">Tuyển dụng</a></li>
+    <li><a href="">Giới thiệu</a></li>
+    <li>
+        <a href="" class = "fab fa-facebook-f"></a>
+        <a href="" class = "fab fa-twitter"></a>
+        <a href="" class = "fab fa-youtube"></a>
+    </li>
+</div>
+<div class="footer-center">
+    <p>Công ty Cổ phần Họa Mi với số đăng ký kinh doanh: 0123456789 <br>
+        Địa chỉ đăng ký: Tổ dân phố 80, P.Hòa Minh, Q.Liên Chiểu, TP.Đà Nẵng, Việt Nam - 0908 080 808<br>
+        Đặt hàng online: <b>0952 648 931.</b>
+    </p>
+</div>
 <script>
 const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
 const imgContainer = document.querySelector('.aspect-ratio-169')
-const dotItem = document.querySelectorAll(".aspect-ratio-169 img")
+const dotItem = document.querySelectorAll(".dot")
 let imgNumber = imgPosition.length
 let index = 0
 imgPosition.forEach(function(image,index){
     image.style.left = index*100 + "%"
+    dotItem[index].addEventListener("click", function(){
+    slider(index)
+    })
 })
 function imgSlide (){
     index++;
     if(index >= imgNumber){index = 0}
+    slider(index)
+}
+function slider(index){
     imgContainer.style.left = "-" + index*100+ "%"
+    const dotActive = document.querySelector('.active')
+    dotActive.classList.remove("active")
+    dotItem[index].classList.add("active")
 }
 setInterval(imgSlide,5000)
 </script>
