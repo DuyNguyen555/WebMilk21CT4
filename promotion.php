21ea1419126efbbf5235f0af1146b1f7eecacf7c
DuyNguyen <drduyvn555@gmail.com>
2023-11-14 15:23:27 +0700

    fix bug

--- promotion.php
+++ promotion.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link rel="stylesheet" href="style/main.css">
     <link rel="stylesheet" href="style/support.css">
-    <link rel="stylesheet" href="style/promotion2.css">
+    <link rel="stylesheet" href="style/promotion.css">
     <title>DTCMilk Việt Nam</title>
 </head>
 <body>
