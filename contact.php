8990ba93ec52d153bbc56c547d142e392f1617da
DuyNguyen <drduyvn555@gmail.com>
2023-11-14 14:56:42 +0700

    fix bug

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link rel="stylesheet" href="style/main.css">
     <link rel="stylesheet" href="style/support.css">
-    <link rel="stylesheet" href="style/contact.css">
+    <link rel="stylesheet" href="style/contact2.css">
     <title>DTCMilk Việt Nam</title>
 </head>
 <body>
