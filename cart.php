e83ad22b46b390017412b39b166048ed1653a19b
DuyNguyen <drduyvn555@gmail.com>
2023-11-15 10:26:20 +0700

    add cart and background

---
+++ cart.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/cart.css">
+    <title>DTCMilk Việt Nam</title>
+</head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="images/logo1.jpg" alt="logo milk">
+            </div>
+            <div class="menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="about.php">Giới thiệu</a></li>
+                <li><a href="">Sản phẩm</a>
+                    <ul class="submenu">
+                        <li><a href="">sữa tươi và sữa dinh dưỡng</a></li>
+                        <li><a href="">sữa cho mẹ bầu và bé</a></li>
+                        <li><a href="">sữa cho người cao tuổi</a></li>
+                        <li><a href="">sữa chua</a></li>
+                        <li><a href="">sữa đặc</a></li>
+                        <li><a href="">sữa thực vật</a></li>
+                    </ul>
+                </li>
+                <li><a href="promotion.php">Khuyến mãi</a></li>
+                <li><a href="contact.php">Liên hệ</a></li>
+            </div>
+            <div class="option">
+                <li><input placeholder="Tìm kiếm" type="text"><i class="fas fa-search"></i></li>
+                <li><a href="login.php" class="fas fa-user"></a></li>
+                <li><a href="cart.php" class="fas fa-shopping-cart"></a></li>
+            </div>
+        </header>
+        <div class="box">
+            <section>
+            </section>
+        </div>
+    </body>
+</html>
