a755bb7905bc9259f821b5160b2ed5e1fcddbc04
DuyNguyen <drduyvn555@gmail.com>
2023-11-21 12:54:14 +0700

    hahaha

--- product.php
+++ product.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link rel="stylesheet" href="style/main2.css">
-    <link rel="stylesheet" href="style/product4.css">
+    <link rel="stylesheet" href="style/product.css">
     <title>DTCMilk Việt Nam</title>
 </head>
 
