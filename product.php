8c0a0894eeed6fbc8e67b4aa4f2d4da3cf95ab8c
DuyNguyen <drduyvn555@gmail.com>
2023-11-22 23:55:47 +0700

    upgrade cart

--- product.php
+++ product.php
@@ -8,22 +8,60 @@
     <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link rel="stylesheet" href="style/main.css">
-    <link rel="stylesheet" href="style/product.css">
+    <link rel="stylesheet" href="style/product2.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>DTCMilk Việt Nam</title>
 </head>
 
 <body>
-<?php
+    <?php
     require_once("sql/connect.php");
 
+    // Sắp xếp sản phẩm
     $sort_order = isset($_GET['sort_order']) && $_GET['sort_order'] === 'desc' ? 'desc' : 'asc';
 
     $sql = "SELECT * FROM sua 
             INNER JOIN hang ON sua.maHang = hang.maHang
             ORDER BY giaTien $sort_order";
-    
+
     $result = mysqli_query($connect, $sql);
-?>
+
+    // Xử lý thêm vào giỏ hàng
+    if (isset($_POST["btnAdd"])) {
+        $productId = $_POST["productId"];
+
+        $query = "SELECT * FROM sua WHERE maSua = '$productId'";
+        $target = mysqli_query($connect, $query);
+        $row2 = mysqli_fetch_assoc($target);
+
+        $productName = $row2["tenSua"];
+        $productCount = 1;
+        $productPrice = $row2["giaTien"];
+        $cart = "INSERT INTO giohang (maSp, tenSp, soLuong, giaTien) VALUES ('$productId', '$productName', $productCount, $productPrice)";
+        $result2 = mysqli_query($connect, $cart);
+
+        if ($result2) {
+            echo '<script>
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Thêm vào giỏ hàng thành công",
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+            </script>';
+        } else {
+            echo '<script>
+                Swal.fire({
+                    position: "top-end",
+                    icon: "warning",
+                    title: "Đã có trong giỏ hàng",
+                });
+            </script>';
+        }
+    }
+
+    ?>
     <header>
         <div class="logo">
             <img src="images/logo1.jpg" alt="logo milk">
@@ -46,9 +84,9 @@
             <li><input placeholder="Tìm kiếm" type="text"><i class="fas fa-search"></i></li>
             <li>
                 <div>
-                <?php
+                    <?php
                     echo $_SESSION["email"];
-                ?>
+                    ?>
                 </div>
                 <a href="login.php" class="fas fa-user"></a>
             </li>
@@ -76,25 +114,26 @@
                     </div>
                 </div>
                 <div class="frame-product-info">
-                <?php 
-                    while($row = mysqli_fetch_assoc($result)){
-                ?>
-                    <div class="product-info">
-                        <form>
-                            <img src="images/product/<?php echo $row["maSua"]; ?>.webp" alt="Ảnh minh hoạ">
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                        <div class="product-info">
+                            <form method="post">
+                                <img src="images/product/<?php echo $row["maSua"]; ?>.webp" alt="Ảnh minh hoạ">
                                 <p class="name"><?php echo $row["tenSua"]; ?></p>
                                 <div class="price-id">
                                     <p class="price"><?php $formattedPrice = number_format($row["giaTien"], 0, ".", ",");
-                                            echo $formattedPrice . " VND" ; ?></p>
-                                    <p class="id-product"><?php echo $row["maSua"] ?></p>
+                                                        echo $formattedPrice . " VND"; ?></p>
+                                    <!-- <p class="id-product"><?php echo $row["maSua"] ?></p> -->
+                                    <input type="text" name="productId" class="" value="<?php echo $row['maSua'] ?>" readonly>
                                 </div>
-                            <input type="submit" value="Thêm vào giỏ hàng">
-                        </form>
-                    </div>
-                <?php
+                                <input type="submit" name="btnAdd" value="Thêm vào giỏ hàng">
+                            </form>
+                        </div>
+                    <?php
                     }
                     mysqli_close($connect);
-                ?>
+                    ?>
                 </div>
             </div>
         </div>
@@ -125,4 +164,5 @@
         </div>
     </section>
 </body>
+
 </html>
