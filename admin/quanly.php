ddf21e61f28829ad50bc541f5eaea7163c8992c7
ThanhHo2205 <thanhcongho880@gmail.com>
2023-11-22 09:29:03 +0700

    add admin

--- admin/quanly.php
+++ admin/quanly.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Quản lý website</title>
 </head>
 <body>
-    
+    <header>
+        <h1>QUẢN LÝ WEBSITE</h1>
+    </header>
+    <section class="admin-content">
+        <div class="admin-content-left">
+
+        </div>
+        <div class="admin-content-left">
+
+    </div>
+    </section>
 </body>
 </html>
