8be3fbdcbeb36d6806931a82696f78b6538d6323
ThanhHo2205 <thanhcongho880@gmail.com>
2023-11-22 11:27:04 +0700

    them quan ly

--- admin/quanly.php
+++ admin/quanly.php
@@ -3,19 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="quanly.css">
     <title>Quản lý website</title>
 </head>
 <body>
     <header>
         <h1>QUẢN LÝ WEBSITE</h1>
     </header>
-    <section class="admin-content">
-        <div class="admin-content-left">
-
+    <div class="admin-content">
+        <div class="content">
+            <div class="option1">
+                <a href="qlpro.php">Quản lý sản phẩm</a>
+            </div>
+            <div class="option2">
+                <a href="#">Quản lý khách hàng</a>
+            </div>
+            <div class="option3">
+                <a href="#">Quản lý đơn hàng</a>
+            </div>
         </div>
-        <div class="admin-content-left">
-
     </div>
-    </section>
 </body>
 </html>
